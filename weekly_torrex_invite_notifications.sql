c70f97f4cb24568fcf40b14fa2f49290fc4ba83d
laumc98 <lanaca98@gmail.com>
2022-03-29 16:35:43 -0500

    changes in notifications - back to general

--- weekly_torrex_invite_notifications.sql
+++ weekly_torrex_invite_notifications.sql
@@ -1,4 +1,4 @@
-select str_to_date(concat(yearweek(no.send_at), ' Sunday'), '%X%V %W') as date, p.subject_identifier as subjid, p.gg_id as ggid
+select str_to_date(concat(yearweek(no.send_at), ' Sunday'), '%X%V %W') as date, count(*)
 from notifications no
-left join people p on no.to = p.id
 where no.template like 'career-advisor-invited-job-opportunity' and no.send_at between date_sub(now(), interval 262 day) and now() and no.status = 'sent'
+group by 1
