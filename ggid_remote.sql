7d529115a95009834743a8662c142d5b8590fd2d
laumc98 <lanaca98@gmail.com>
2022-03-29 11:36:31 -0500

    changes in id for notifications

--- ggid_remote.sql
+++ ggid_remote.sql
@@ -1,4 +1,4 @@
-SELECT str_to_date(concat(yearweek(`opportunities`.`created`), ' Sunday'), '%X%V %W') AS `created_date_remote`, `People - Applicant Coordinator Person`.`gg_id` AS `ggid`
+SELECT str_to_date(concat(yearweek(`opportunities`.`created`), ' Sunday'), '%X%V %W') AS `created_date_remote`, `People - Applicant Coordinator Person`.`subject_identifier` AS `subjid`, `People - Applicant Coordinator Person`.`gg_id` AS `ggid`
 FROM `opportunities`
 LEFT JOIN `people` `People - Applicant Coordinator Person` ON `opportunities`.`applicant_coordinator_person_id` = `People - Applicant Coordinator Person`.`id`
 WHERE `opportunities`.`remote` = 1 AND `People - Applicant Coordinator Person`.`gg_id` IS NOT NULL AND `opportunities`.`created` BETWEEN date_sub(now(), interval 272 day) and now()
