6495a7340724a1ed2fc43f739a05a8f9be5aeba0
laumc98 <lanaca98@gmail.com>
2022-03-29 17:18:18 -0500

    update notifications names columns

--- weekly_torrex_invite_notifications.sql
+++ weekly_torrex_invite_notifications.sql
@@ -1,4 +1,4 @@
-select str_to_date(concat(yearweek(no.send_at), ' Sunday'), '%X%V %W') as date, count(*)
+select str_to_date(concat(yearweek(no.send_at), ' Sunday'), '%X%V %W') as date, count(*) as count_trx_invite
 from notifications no
 where no.template like 'career-advisor-invited-job-opportunity' and no.send_at between date_sub(now(), interval 262 day) and now() and no.status = 'sent'
 group by 1
