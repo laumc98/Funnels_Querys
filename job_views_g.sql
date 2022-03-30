f67c420f0a2a4ec17a644735e450ac53661ad4a4
laumc98 <lanaca98@gmail.com>
2022-03-30 13:53:11 -0500

    Update job_views_g.sql

--- job_views_g.sql
+++ job_views_g.sql
@@ -3,5 +3,5 @@ from views v
 inner join opportunities o on o.id = v.target_id
 inner join opportunity_members omp on omp.opportunity_id = v.target_id 
 inner join person_flags pf on pf.person_id = omp.person_id 
-where o.objective not like '***%' and o.remote = 1 and omp.poster = true and pf.opportunity_crawler = false 
+where o.objective not like '***%' and o.remote = 1 and omp.poster = true and pf.opportunity_crawler = false and v.target_type = 'opportunity' and v.created between date_sub(now(), interval 262 day) and now() 
 group by 1,2
