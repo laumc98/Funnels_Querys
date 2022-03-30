b0eeea80ba11bcf474706041339fcfaee9a8792a
laumc98 <lanaca98@gmail.com>
2022-03-30 13:49:33 -0500

    Create job_views_g.sql

---
+++ job_views_g.sql
@@ -0,0 +1,7 @@
+select year(v.created) as year, week(v.created, 0) as week, date(v.created) as created, count(*) as weekly 
+from views v 
+inner join opportunities o on o.id = v.target_id
+inner join opportunity_members omp on omp.opportunity_id = v.target_id 
+inner join person_flags pf on pf.person_id = omp.person_id 
+where o.objective not like '***%' and o.remote = 1 and omp.poster = true and pf.opportunity_crawler = false 
+group by 1,2
