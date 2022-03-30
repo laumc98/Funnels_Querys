9819f5014d601f6f53eb53bb01445cc2a9706ad2
laumc98 <lanaca98@gmail.com>
2022-03-30 13:32:07 -0500

    Update jobs_views.sql

--- jobs_views.sql
+++ jobs_views.sql
@@ -1,4 +1,4 @@
-SELECT date(`Views`.`created`) AS date_views, `Views`.`target_id` AS id
+SELECT str_to_date(concat(yearweek(`Views`.`created`), ' Sunday'), '%X%V %W') AS date_views,`Views`.`target_id` as id
 FROM `opportunities`
 LEFT JOIN `views` `Views` ON `opportunities`.`id` = `Views`.`target_id`
 WHERE `Views`.`target_type` = 'opportunity' AND `opportunities`.`remote` = 1 AND `Views`.`created` between date_sub(now(), interval 262 day) AND now()
