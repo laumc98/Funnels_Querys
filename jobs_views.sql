15cc48dfbd6601e738da7e6d6242a95d2ce1bf5a
laumc98 <lanaca98@gmail.com>
2022-03-30 11:53:18 -0500

    Create jobs_views.sql

---
+++ jobs_views.sql
@@ -0,0 +1,5 @@
+SELECT date(`Views`.`created`) AS date_views, `Views`.`target_id` AS id
+FROM `opportunities`
+LEFT JOIN `views` `Views` ON `opportunities`.`id` = `Views`.`target_id`
+WHERE `Views`.`target_type` = 'opportunity' AND `opportunities`.`remote` = 1 AND `Views`.`created` between date_sub(now(), interval 262 day) AND now()
+LIMIT 1048575
