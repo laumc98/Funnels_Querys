54e74c9fa62165e036f0e299f3557a303d124e48
laumc98 <lanaca98@gmail.com>
2022-03-31 16:21:20 -0500

    update src date

--- weekly_sourcing_request.sql
+++ weekly_sourcing_request.sql
@@ -1,4 +1,4 @@
 select str_to_date(concat(yearweek(no.send_at), ' Sunday'), '%X%V %W') as date, count(*) as count_sourcing
 from notifications no
-WHERE no.template like 'career-advisor-sourcing-first-evaluation' and no.send_at > "2021-12-18"
+WHERE no.template like 'career-advisor-sourcing-first-evaluation' and no.send_at > "2022-2-6"
 group by 1
