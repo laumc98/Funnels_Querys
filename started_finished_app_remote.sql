af2ea7e86900ffe725406813b10cefdb542bf50b
laumc98 <lanaca98@gmail.com>
2022-04-02 10:45:05 -0500

    Update started_finished_app_remote.sql

--- started_finished_app_remote.sql
+++ started_finished_app_remote.sql
@@ -9,6 +9,6 @@ left join people on opportunity_members.person_id = people.id
 left join person_flags on people.id = person_flags.person_id
 where  date(opportunity_candidates.created) between date_sub(now(), interval 262 day) and now() AND
 o.objective not like '***%'
-and tc.utm_medium in ('srh_jobs', 'ja_mtc', 'am_sug', 'rc_cb_rcdt', 'rc_trrx_inv','rc_ccg','ro_sug', 'rc_syn', 'rc_src', 'pr_sml_jobs','syn','src','sml_jobs','trr_crg','trr_webinars','google_jobs','syn_rqt','shr_ts','ref_ts','ref_ptn','ref_cdt','ref_vst_imp','ja_rlvsgl_prs','ja_allsgl_prs','ja_rlvsgl_org','ja_allsgl_org')
+and tc.utm_medium in ('srh_jobs','am_inv','ja_mtc', 'am_sug', 'rc_cb_rcdt', 'rc_trrx_inv','rc_ccg','ro_sug', 'rc_syn', 'rc_src', 'pr_sml_jobs','syn','src','sml_jobs','trr_crg','trr_webinars','google_jobs','syn_rqt','shr_ts','ref_ts','ref_ptn','ref_cdt','ref_vst_imp','ja_rlvsgl_prs','ja_allsgl_prs','ja_rlvsgl_org','ja_allsgl_org')
 and (o.remote = 1)
 group by 1, 2,3
