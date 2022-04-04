7677fae6d4c932ef4bd13da3aa1e55839b434b18
laumc98 <lanaca98@gmail.com>
2022-04-04 16:01:53 -0500

    Update weekly_recruiter_ccgs_notifications.sql

--- weekly_recruiter_ccgs_notifications.sql
+++ weekly_recruiter_ccgs_notifications.sql
@@ -1,7 +1,6 @@
-SELECT str_to_date(concat(yearweek(`notifications`.`send_at`), ' Sunday'), '%X%V %W') AS `date`, count(*) AS `count_ccgs`
+SELECT str_to_date(concat(yearweek(`notifications`.`send_at`), ' Sunday'), '%X%V %W') AS `date`, TRIM('"' FROM JSON_EXTRACT(`notifications`.`context`, '$.opportunityId')) as AlfaID
 FROM `notifications` INNER JOIN `person_flags` `Person Flags - To` ON `notifications`.`to` = `Person Flags - To`.`person_id`
 WHERE (`notifications`.`template` = 'career-advisor-job-opportunity'
    AND `notifications`.`status` = 'sent'
    AND `Person Flags - To`.`community_created_claimed_at` is null
    AND `notifications`.`send_at` between date_sub(now(), interval 262 day) and now())
-GROUP BY 1
