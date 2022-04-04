64811cae01a64342e498328a272e281b1e11e507
laumc98 <lanaca98@gmail.com>
2022-04-04 12:27:47 -0500

    changes in nyax remote notifications

--- ggid_remote.sql
+++
@@ -1,5 +0,0 @@
-SELECT `opportunity_candidates`.`created` AS `created`, `People`.`gg_id` AS `People__gg_id`
-FROM `opportunity_candidates`
-LEFT JOIN `people` `People` ON `opportunity_candidates`.`person_id` = `People`.`id` LEFT JOIN `opportunities` `Opportunities` ON `opportunity_candidates`.`opportunity_id` = `Opportunities`.`id`
-WHERE `Opportunities`.`remote` = 1 and `opportunity_candidates`.`created` between date_sub(now(), interval 262 day) and now() 
-LIMIT 1048575
---
+++ oppsid_remote.sql
@@ -0,0 +1,4 @@
+SELECT `opportunities`.`id` AS `id`
+FROM `opportunities`
+WHERE `opportunities`.`remote` = TRUE
+LIMIT 1048575
