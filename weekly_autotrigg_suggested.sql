4ce8b7ebfe1ec295ab31d4100437f945148bc228
laumc98 <lanaca98@gmail.com>
2022-04-04 17:56:11 -0500

    Update weekly_autotrigg_suggested.sql

--- weekly_autotrigg_suggested.sql
+++ weekly_autotrigg_suggested.sql
@@ -6,11 +6,11 @@ FROM
     left join automated_messages as am on (oam.automated_message_id = am.id)
     left join opportunity_columns as oc on (oam.column_id = oc.id)
     left join automated_messages_notifications as amn on (am.id = amn.automated_message_id)
-    left join people as sender on (amn.from_person_id = sender.id)
-    left join people as receiver on (amn.to_person_id = receiver.id)
+    inner join opportunities o on (oam.opportunity_id = o.id)
 WHERE
     oam.active = true 
     and am.active = true
     and amn.status = 'sent'
     and coalesce(amn.sent_at, amn.send_at) between date_sub(now(), interval 262 day) and now()
+    and o.remote = 1
 GROUP BY 1
