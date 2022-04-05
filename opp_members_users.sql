48dfaf7c0fba4a67118cf7930b23a8c30782420d
laumc98 <lanaca98@gmail.com>
2022-04-05 15:12:41 -0500

    change columns names in sharings querys

--- opp_members_users.sql
+++ opp_members_users.sql
@@ -1,4 +1,4 @@
-SELECT `opportunity_members`.`opportunity_id` AS `opportunity_id`,`People`.`gg_id` AS `People__gg_id`
+SELECT `opportunity_members`.`opportunity_id` AS `ID`,`People`.`gg_id` AS `gg_id`
 FROM `opportunity_members` INNER JOIN `opportunities` `Opportunities` ON `opportunity_members`.`opportunity_id` = `Opportunities`.`id`
 LEFT JOIN `people` `People` ON `opportunity_members`.`person_id` = `People`.`id`
 WHERE (`Opportunities`.`remote` = TRUE
--- sharings.sql
+++ sharings.sql
@@ -1,4 +1,4 @@
-SELECT "atomic"."com_torrelabs_referrer_token_generated_1"."root_tstamp" AS "root_tstamp", "atomic"."com_torrelabs_referrer_token_generated_1"."user_gg_id" AS "user_gg_id",SUBSTRING("atomic"."com_torrelabs_referrer_token_generated_1"."action" , 13, 20) AS "AlfaID" 
+SELECT "atomic"."com_torrelabs_referrer_token_generated_1"."root_tstamp" AS "date", "atomic"."com_torrelabs_referrer_token_generated_1"."user_gg_id" AS "gg_id",SUBSTRING("atomic"."com_torrelabs_referrer_token_generated_1"."action" , 13, 20) AS "AlfaID" 
 FROM "atomic"."com_torrelabs_referrer_token_generated_1"
 LEFT JOIN "atomic"."com_torrelabs_user_context_1" "Com Torrelabs User Context 1 - Root" ON "atomic"."com_torrelabs_referrer_token_generated_1"."root_id" = "Com Torrelabs User Context 1 - Root"."root_id"
 WHERE "Com Torrelabs User Context 1 - Root"."is_test" = 0 
