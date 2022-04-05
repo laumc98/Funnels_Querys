849f6eb4394ed4b26d3203b0a8d21b36377d6238
laumc98 <lanaca98@gmail.com>
2022-04-05 16:42:55 -0500

    Update opp_members_users.sql

--- opp_members_users.sql
+++ opp_members_users.sql
@@ -1,6 +1,5 @@
 SELECT `opportunity_members`.`opportunity_id` AS `id`,`People`.`gg_id` AS `gg_id`
 FROM `opportunity_members` INNER JOIN `opportunities` `Opportunities` ON `opportunity_members`.`opportunity_id` = `Opportunities`.`id`
 LEFT JOIN `people` `People` ON `opportunity_members`.`person_id` = `People`.`id`
-WHERE (`Opportunities`.`remote` = TRUE
-   AND `opportunity_members`.`member` = TRUE)
+WHERE (`opportunity_members`.`member` = TRUE)
 LIMIT 1048575
