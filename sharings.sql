2a2cff087eccbd67b6149e2c338ad80f21f299fc
laumc98 <lanaca98@gmail.com>
2022-04-05 18:37:32 -0500

    Changes in date fields

--- sharings.sql
+++ sharings.sql
@@ -1,10 +1,11 @@
-SELECT str_to_date(concat(yearweek("atomic"."com_torrelabs_referrer_token_generated_1"."root_tstamp"), ' Sunday'), '%X%V %W') AS "date", "atomic"."com_torrelabs_referrer_token_generated_1"."user_gg_id" AS "gg_id",SUBSTRING("atomic"."com_torrelabs_referrer_token_generated_1"."action" , 13, 20) AS "AlfaID" 
+SELECT (CAST(date_trunc('week', CAST(("atomic"."com_torrelabs_referrer_token_generated_1"."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day'))  AS "date", "atomic"."com_torrelabs_referrer_token_generated_1"."user_gg_id" AS "gg_id",SUBSTRING("atomic"."com_torrelabs_referrer_token_generated_1"."action" , 13, 20) AS "AlfaID" 
 FROM "atomic"."com_torrelabs_referrer_token_generated_1"
 LEFT JOIN "atomic"."com_torrelabs_user_context_1" "Com Torrelabs User Context 1 - Root" ON "atomic"."com_torrelabs_referrer_token_generated_1"."root_id" = "Com Torrelabs User Context 1 - Root"."root_id"
-WHERE "Com Torrelabs User Context 1 - Root"."is_test" = 0 
-   AND ((lower("atomic"."com_torrelabs_referrer_token_generated_1"."action") like 'opportunity%')
+WHERE ( "Com Torrelabs User Context 1 - Root"."is_test" = 0 
+   AND (lower("atomic"."com_torrelabs_referrer_token_generated_1"."action") like 'opportunity%')
    AND (NOT (lower("atomic"."com_torrelabs_referrer_token_generated_1"."action") like '%draft%')
     OR "atomic"."com_torrelabs_referrer_token_generated_1"."action" IS NULL) 
    AND (NOT (lower("atomic"."com_torrelabs_referrer_token_generated_1"."action") like '%undefined%') 
-    OR "atomic"."com_torrelabs_referrer_token_generated_1"."action" IS NULL))
-   AND "atomic"."com_torrelabs_referrer_token_generated_1"."root_tstamp" between date_sub(now(), interval 262 day) and now()
+    OR "atomic"."com_torrelabs_referrer_token_generated_1"."action" IS NULL)
+   AND "atomic"."com_torrelabs_referrer_token_generated_1"."root_tstamp" >= CAST(dateadd('day', -272, CAST(getdate() AS timestamp)) AS date)
+   AND "atomic"."com_torrelabs_referrer_token_generated_1"."root_tstamp" < CAST(getdate() AS date))
--- similars_jobs.sql
+++ similars_jobs.sql
@@ -1,3 +1,5 @@
-SELECT str_to_date(concat(yearweek("atomic"."com_torrelabs_similar_opportunity_clicked_1"."root_tstamp"), ' Sunday'), '%X%V %W') AS "date", "atomic"."com_torrelabs_similar_opportunity_clicked_1"."clicked_opportunity_id" AS "clicked_opportunity_id", "atomic"."com_torrelabs_similar_opportunity_clicked_1"."opportunity_id" AS "AlfaID"
+SELECT (CAST(date_trunc('week', CAST(("atomic"."com_torrelabs_similar_opportunity_clicked_1"."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) AS "date", "atomic"."com_torrelabs_similar_opportunity_clicked_1"."clicked_opportunity_id" AS "clicked_opportunity_id", "atomic"."com_torrelabs_similar_opportunity_clicked_1"."opportunity_id" AS "AlfaID"
 FROM "atomic"."com_torrelabs_similar_opportunity_clicked_1"
-WHERE "atomic"."com_torrelabs_similar_opportunity_clicked_1"."element_type" = 'job-preview-card' AND "atomic"."com_torrelabs_similar_opportunity_clicked_1"."root_tstamp" between date_sub(now(), interval 262 day) and now()
+WHERE ("atomic"."com_torrelabs_similar_opportunity_clicked_1"."element_type" = 'job-preview-card'
+      AND "atomic"."com_torrelabs_similar_opportunity_clicked_1"."root_tstamp"  >= CAST(dateadd('day', -272, CAST(getdate() AS timestamp)) AS date)
+      AND "atomic"."com_torrelabs_similar_opportunity_clicked_1"."root_tstamp"  < CAST(getdate() AS date))
