f8330310b5629fc62ffefff649299b1f6e304b5a
laumc98 <lanaca98@gmail.com>
2022-04-13 11:05:33 -0500

    Update weekly_autotrigg_external.sql

--- weekly_autotrigg_external.sql
+++ weekly_autotrigg_external.sql
@@ -1,6 +1,14 @@
-SELECT str_to_date(concat(yearweek(`notifications`.`send_at`), ' Sunday'), '%X%V %W') AS `date`, count(*) AS `count_trigg_ext`
-FROM `notifications`
-WHERE (`notifications`.`template` = 'nps'
-   AND `notifications`.`status` = 'sent'
-   AND `notifications`.`send_at` between date_sub(now(), interval 262 day) and now())
-GROUP BY 1
+select str_to_date(concat(yearweek(oc.created), ' Sunday'), '%X%V %W') as date, count(*) as 'count_trigg_ext'
+from opportunity_candidates oc
+    inner join member_evaluations me on me.candidate_id = oc.id 
+    inner join people p on oc.person_id = p.id
+    inner join opportunities o on oc.opportunity_id = o.id
+where (me.interested is not null 
+    and me.not_interested is null
+    and me.send_disqualified_notification = false
+    and oc.column_id is null
+    and oc.application_step is null
+    and o.remote = 1
+    and oc.created between date_sub(now(), interval 262 day) and now())
+group by 1
+    
