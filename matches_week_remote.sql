f6a9bb0cc1ac9d6210395a0d24af0639fe11cc6e
laumc98 <lanaca98@gmail.com>
2022-04-28 17:47:25 -0500

    Update matches_week_remote.sql

--- matches_week_remote.sql
+++ matches_week_remote.sql
@@ -1,6 +1,6 @@
-select (CAST(date_trunc('week', CAST(("atomic"."com_torrelabs_match_distributed_2"."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) as date, count(*), model
-from "atomic"."com_torrelabs_match_distributed_2"
-WHERE ("atomic"."com_torrelabs_match_distributed_2"."root_tstamp" >= CAST(dateadd('day', -272, CAST(getdate() AS timestamp)) AS date)
-   AND "atomic"."com_torrelabs_match_distributed_2"."root_tstamp" < CAST(getdate() AS date))
-   AND "atomic"."com_torrelabs_match_distributed_2"."opportunity_remote" = 1
+select (CAST(date_trunc('week', CAST(("atomic"."com_torrelabs_match_distributed_3"."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) as date, count(*), model
+from "atomic"."com_torrelabs_match_distributed_3"
+WHERE ("atomic"."com_torrelabs_match_distributed_3"."root_tstamp" >= CAST(dateadd('day', -272, CAST(getdate() AS timestamp)) AS date)
+   AND "atomic"."com_torrelabs_match_distributed_3"."root_tstamp" < CAST(getdate() AS date))
+   AND "atomic"."com_torrelabs_match_distributed_3"."opportunity_remote" = 1
 group by model, 1
