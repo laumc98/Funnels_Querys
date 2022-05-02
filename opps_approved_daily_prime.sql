3263fe645cb48bac37945953c7cdc9b3826a405c
laumc98 <lanaca98@gmail.com>
2022-05-02 16:03:12 -0500

    add daily metrics for job post stats

---
+++ opps_approved_daily_prime.sql
@@ -0,0 +1,22 @@
+SELECT
+    date(`opportunities`.`reviewed`) AS `date`,
+    count(distinct `opportunities`.`id`) AS `opps_approved_weekly_prime`
+FROM
+    `opportunities`
+    LEFT JOIN `opportunity_organizations` `Opportunity Organizations` ON `opportunities`.`id` = `Opportunity Organizations`.`opportunity_id`
+WHERE
+    (
+        `opportunities`.`reviewed` IS NOT NULL
+        AND `opportunities`.`reviewed` > "2021-7-18"
+        AND `opportunities`.`reviewed` < date(date_add(now(6), INTERVAL 1 day))
+        AND `opportunities`.`review` = 'approved'
+        AND (
+            `Opportunity Organizations`.`organization_id` <> 665801
+            OR `Opportunity Organizations`.`organization_id` IS NULL
+        )
+        AND `opportunities`.`fulfillment` = 'prime'
+    )
+GROUP BY
+    date(`opportunities`.`reviewed`)
+ORDER BY
+    date(`opportunities`.`reviewed`) ASC
---
+++ opps_commited_daily_remote.sql
@@ -0,0 +1,20 @@
+SELECT
+    date(`opportunity_changes_history`.`created`) AS `date`,
+    count(
+        distinct `opportunity_changes_history`.`opportunity_id`
+    ) AS `opps_commited_weekly_remote`
+FROM
+    `opportunity_changes_history`
+    LEFT JOIN `opportunities` `opportunities__via__opportunit` ON `opportunity_changes_history`.`opportunity_id` = `opportunities__via__opportunit`.`id`
+WHERE
+    (
+        `opportunity_changes_history`.`type` = 'outbound'
+        AND `opportunity_changes_history`.`value` = 0
+        AND `opportunities__via__opportunit`.`reviewed` > "2021-7-18"
+        AND `opportunities__via__opportunit`.`reviewed` < date(now(6))
+        AND `opportunities__via__opportunit`.`remote` = TRUE
+    )
+GROUP BY
+    date(`opportunity_changes_history`.`created`)
+ORDER BY
+    date(`opportunity_changes_history`.`created`) ASC
