4e1568393614ede5cd9304e0e62e00c73f6718c8
laumc98 <lanaca98@gmail.com>
2022-05-26 10:13:38 -0500

    changes in notifications from autotrigg sugg

--- daily_autotrigg_suggested.sql
+++ daily_autotrigg_suggested.sql
@@ -1,16 +1,15 @@
-SELECT 
-    date(coalesce(amn.sent_at, amn.send_at)) as 'daily_date',
-    count(*) as 'count_trigg_sugg'
-FROM  
-    opportunity_automated_messages as oam 
-    left join automated_messages as am on (oam.automated_message_id = am.id)
-    left join opportunity_columns as oc on (oam.column_id = oc.id)
-    left join automated_messages_notifications as amn on (am.id = amn.automated_message_id)
-    inner join opportunities o on (oam.opportunity_id = o.id)
+SELECT
+    date(`notifications`.`sent_at`) AS `daily_date`,
+    count(*) AS `count_trigg_sugg`
+FROM
+    `notifications`
 WHERE
-    oam.active = true 
-    and am.active = true
-    and amn.status = 'sent'
-    and coalesce(amn.sent_at, amn.send_at) > '2022-01-1'
-    and o.remote = 1
-GROUP BY 1
+    (
+        (
+            `notifications`.`template` = 'talent-candidate-manually-invited'
+            OR `notifications`.`template` = 'talent-candidate-invited'
+        )
+        AND `notifications`.`status` = 'sent'
+        AND `notifications`.`sent_at` >= '2022-01-1'
+    )
+GROUP BY date(`notifications`.`sent_at`)
--- weekly_autotrigg_suggested.sql
+++ weekly_autotrigg_suggested.sql
@@ -1,16 +1,15 @@
-SELECT 
-    str_to_date(concat(yearweek(coalesce(amn.sent_at, amn.send_at)), ' Sunday'), '%X%V %W') as 'date',
-    count(*) as 'count_trigg_sugg'
-FROM  
-    opportunity_automated_messages as oam 
-    left join automated_messages as am on (oam.automated_message_id = am.id)
-    left join opportunity_columns as oc on (oam.column_id = oc.id)
-    left join automated_messages_notifications as amn on (am.id = amn.automated_message_id)
-    inner join opportunities o on (oam.opportunity_id = o.id)
+SELECT
+    str_to_date(concat(yearweek(`notifications`.`sent_at`), ' Sunday'),'%X%V %W') AS `date`,
+    count(*) AS `count_trigg_sugg`
+FROM
+    `notifications`
 WHERE
-    oam.active = true 
-    and am.active = true
-    and amn.status = 'sent'
-    and coalesce(amn.sent_at, amn.send_at) > '2021-08-15'
-    and o.remote = 1
+    (
+        (
+            `notifications`.`template` = 'talent-candidate-manually-invited'
+            OR `notifications`.`template` = 'talent-candidate-invited'
+        )
+        AND `notifications`.`status` = 'sent'
+        AND `notifications`.`sent_at` >= '2021-08-15'
+    )
 GROUP BY 1
