72fdb92dd505f00e0916201eed1320cc9d2037df
laumc98 <lanaca98@gmail.com>
2022-06-14 11:35:04 -0500

    views

---
+++ daily_views_from_notifications.sql
@@ -0,0 +1,37 @@
+SELECT
+    CAST("atomic"."events"."derived_tstamp" AS date) AS "daily_date",
+    SUBSTRING("atomic"."events"."page_urlpath",7,8) as "AlfaID",
+    "atomic"."events"."mkt_medium" AS "UTM",
+    count(*) AS "daily_views_notifications"
+FROM
+    "atomic"."events"
+WHERE
+    (
+        "atomic"."events"."event" = 'page_view'
+        AND (
+            lower("atomic"."events"."page_url") like '%post%'
+        )
+        AND (
+            "atomic"."events"."mkt_medium" = 'am_sug'
+            OR "atomic"."events"."mkt_medium" = 'srh_jobs'
+            OR "atomic"."events"."mkt_medium" = 'ja_mtc'
+            OR "atomic"."events"."mkt_medium" = 'ja_allsgl_org'
+            OR "atomic"."events"."mkt_medium" = 'ja_allsgl_prs'
+            OR "atomic"."events"."mkt_medium" = 'ja_rlvsgl_prs'
+            OR "atomic"."events"."mkt_medium" = 'ja_rlvsgl_org'
+            OR "atomic"."events"."mkt_medium" = 'rc_cb_rcdt'
+            OR "atomic"."events"."mkt_medium" = 'sml_jobs'
+            OR "atomic"."events"."mkt_medium" = 'am_inv'
+            OR "atomic"."events"."mkt_medium" = 'rc_am_sug'
+            OR "atomic"."events"."mkt_medium" = 'rc_trrx_inv'
+            OR "atomic"."events"."mkt_medium" = 'syn'
+            OR "atomic"."events"."mkt_medium" = 'rc_syn'
+            OR "atomic"."events"."mkt_medium" = 'rc_src'
+            OR "atomic"."events"."mkt_medium" = 'rc_ccg'
+            OR "atomic"."events"."mkt_medium" = 'ref_ts'
+        )
+    )
+GROUP BY
+    1,2,3
+ORDER BY
+    CAST("atomic"."events"."derived_tstamp" AS date) ASC
---
+++ weekly_views_from_notifications.sql
@@ -0,0 +1,39 @@
+SELECT
+    (CAST(date_trunc('week',CAST(("atomic"."events"."derived_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) AS "date",
+    SUBSTRING("atomic"."events"."page_urlpath",7,8) as "AlfaID",
+    "atomic"."events"."mkt_medium" AS "UTM",
+    count(*) AS "weekly_views_notifications"
+FROM
+    "atomic"."events"
+WHERE
+    (
+        "atomic"."events"."event" = 'page_view'
+        AND (
+            lower("atomic"."events"."page_url") like '%post%'
+        )
+        AND (
+            "atomic"."events"."mkt_medium" = 'am_sug'
+            OR "atomic"."events"."mkt_medium" = 'srh_jobs'
+            OR "atomic"."events"."mkt_medium" = 'ja_mtc'
+            OR "atomic"."events"."mkt_medium" = 'ja_allsgl_org'
+            OR "atomic"."events"."mkt_medium" = 'ja_allsgl_prs'
+            OR "atomic"."events"."mkt_medium" = 'ja_rlvsgl_prs'
+            OR "atomic"."events"."mkt_medium" = 'ja_rlvsgl_org'
+            OR "atomic"."events"."mkt_medium" = 'rc_cb_rcdt'
+            OR "atomic"."events"."mkt_medium" = 'sml_jobs'
+            OR "atomic"."events"."mkt_medium" = 'am_inv'
+            OR "atomic"."events"."mkt_medium" = 'rc_am_sug'
+            OR "atomic"."events"."mkt_medium" = 'rc_trrx_inv'
+            OR "atomic"."events"."mkt_medium" = 'syn'
+            OR "atomic"."events"."mkt_medium" = 'rc_syn'
+            OR "atomic"."events"."mkt_medium" = 'rc_src'
+            OR "atomic"."events"."mkt_medium" = 'rc_ccg'
+            OR "atomic"."events"."mkt_medium" = 'ref_ts'
+        )
+    )
+GROUP BY
+    (CAST(date_trunc('week',CAST(("atomic"."events"."derived_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')),
+    SUBSTRING("atomic"."events"."page_urlpath",7,8),
+    "atomic"."events"."mkt_medium"
+ORDER BY
+    (CAST(date_trunc('week',CAST(("atomic"."events"."derived_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) ASC
