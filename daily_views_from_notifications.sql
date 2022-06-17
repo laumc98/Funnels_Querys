0c4829a33c6bb4c30d09b10b27d4b4ff3ce57b75
laumc98 <lanaca98@gmail.com>
2022-06-17 10:37:23 -0500

    changes in views

--- daily_views_from_notifications.sql
+++ daily_views_from_notifications.sql
@@ -2,7 +2,7 @@ SELECT
     CAST("atomic"."events"."collector_tstamp" AS date) AS "daily_date",
     "atomic"."events"."mkt_medium" AS "UTM",
     SUBSTRING("atomic"."events"."page_urlpath",7,8) as "AlfaID",
-    count(*) AS "daily_views_notifications"
+    count(distinct "atomic"."events"."domain_userid") AS "daily_views_notifications"
 FROM
     "atomic"."events"
 WHERE
@@ -35,7 +35,6 @@ WHERE
 GROUP BY
     CAST("atomic"."events"."collector_tstamp" AS date),
     "atomic"."events"."mkt_medium",
-    SUBSTRING("atomic"."events"."page_urlpath",7,8),
-    "atomic"."events"."domain_userid"
+    SUBSTRING("atomic"."events"."page_urlpath",7,8)
 ORDER BY
     CAST("atomic"."events"."collector_tstamp" AS date) ASC
--- weekly_views_from_notifications.sql
+++ weekly_views_from_notifications.sql
@@ -2,7 +2,7 @@ SELECT
     (CAST(date_trunc('week',CAST(("atomic"."events"."collector_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) AS "date",
     "atomic"."events"."mkt_medium" AS "UTM",
     SUBSTRING("atomic"."events"."page_urlpath",7,8) as "AlfaID",
-    count(*) AS "weekly_views_notifications"
+    count(distinct "atomic"."events"."domain_userid") AS "weekly_views_notifications"
 FROM
     "atomic"."events"
 WHERE
@@ -35,7 +35,6 @@ WHERE
 GROUP BY
     (CAST(date_trunc('week',CAST(("atomic"."events"."collector_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')),
     "atomic"."events"."mkt_medium",
-    SUBSTRING("atomic"."events"."page_urlpath",7,8),
-    "atomic"."events"."domain_userid"
+    SUBSTRING("atomic"."events"."page_urlpath",7,8)
 ORDER BY
     (CAST(date_trunc('week',CAST(("atomic"."events"."collector_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) ASC
