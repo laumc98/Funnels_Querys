24d8195d22a3ffed46cc26606029a11258c9e3ca
laumc98 <lanaca98@gmail.com>
2022-06-23 13:42:24 -0500

    Update weekly_autotrigg_suggested.sql

--- weekly_autotrigg_suggested.sql
+++ weekly_autotrigg_suggested.sql
@@ -10,6 +10,6 @@ WHERE
             `notifications`.`template` = 'talent-candidate-manually-invited'
         )
         AND `notifications`.`status` = 'sent'
-        AND `notifications`.`sent_at` >= '2021-08-15'
+        AND `notifications`.`sent_at` >= '2021-09-01'
     )
 GROUP BY 1
