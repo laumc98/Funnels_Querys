d47fe7c2e24773417b06409ff96189e568512cfd
laumc98 <lanaca98@gmail.com>
2022-08-16 11:06:41 -0500

    Create not_interested_sourcing.sql

---
+++ not_interested_sourcing.sql
@@ -0,0 +1,11 @@
+/* AA : Channel's performance :daily not interested users from sourcing: prod */ 
+SELECT
+    date(career_advisor.created) AS "daily_date",
+    count(distinct career_advisor.person_id) AS "not_interested"
+FROM
+    career_advisor
+WHERE
+    career_advisor.current = 'career-advisor-sourcing-not-interested-1'
+    AND career_advisor.created >= '2022-06-28'
+GROUP BY
+    date(career_advisor.created)
