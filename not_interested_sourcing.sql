85d36378774ed4384ec1d15a8f96e4d49bf1fb7a
laumc98 <lanaca98@gmail.com>
2022-08-17 14:50:20 -0500

    Update not_interested_sourcing.sql

--- not_interested_sourcing.sql
+++ not_interested_sourcing.sql
@@ -1,11 +1,17 @@
 /* AA : Channel's performance :daily not interested users from sourcing: prod */ 
 SELECT
-    date(career_advisor.created) AS "daily_date",
-    count(distinct career_advisor.person_id) AS "not_interested"
-FROM
-    career_advisor
-WHERE
-    career_advisor.current = 'career-advisor-sourcing-not-interested-1'
-    AND career_advisor.created >= '2022-06-28'
-GROUP BY
-    date(career_advisor.created)
+    str_to_date(concat(yearweek(users.date), ' Sunday'),'%X%V %W') AS date,
+    date(users.date) AS daily_date,
+    users.not_interested AS not_interested
+FROM(
+    SELECT
+        date(career_advisor.created) AS "date",
+        count(distinct career_advisor.person_id) AS "not_interested"
+    FROM
+        career_advisor
+    WHERE
+        career_advisor.current = 'career-advisor-sourcing-not-interested-1'
+        AND career_advisor.created >= '2022-06-28'
+    GROUP BY
+        date(career_advisor.created)
+) users
