158b1026881c2d433681137b670e542de627c1b9
laumc98 <lanaca98@gmail.com>
2022-10-11 17:55:55 -0500

    Update matches_week_remote.sql

--- matches_week_remote.sql
+++ matches_week_remote.sql
@@ -1,11 +1,12 @@
 /* AA : Channel's performance : weekly job matches remote : prod */ 
 SELECT
-    (CAST(date_trunc('week',CAST(("snowplow"."com_torrelabs_match_distributed_3"."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) as date,
+    date("snowplow"."com_torrelabs_match_distributed_3"."root_tstamp") as date,
     model,
     opportunity_ref AS AlfaID
 FROM
     "snowplow"."com_torrelabs_match_distributed_3"
 WHERE
     (
-        "snowplow"."com_torrelabs_match_distributed_3"."root_tstamp" >= '2021-07-17'
+        "snowplow"."com_torrelabs_match_distributed_3"."root_tstamp" >= '2021-08-17'
     )
+GROUP BY 1,2,3
