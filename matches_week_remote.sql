15dfd7a3d26752b69b047e2f7d043813f809ed4c
laumc98 <lanaca98@gmail.com>
2022-10-11 14:16:37 -0500

    Update matches_week_remote.sql

--- matches_week_remote.sql
+++ matches_week_remote.sql
@@ -10,6 +10,6 @@ WHERE
         "snowplow"."com_torrelabs_match_distributed_3"."root_tstamp" >= '2021-07-17'
     )
 GROUP BY
-    (CAST(date_trunc('week',CAST(("snowplow"."com_torrelabs_match_distributed_3"."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) as date,
+    (CAST(date_trunc('week',CAST(("snowplow"."com_torrelabs_match_distributed_3"."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')),
     model,
     opportunity_ref
