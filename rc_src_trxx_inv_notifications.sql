fc40d028912854ec4ff8e96821dcacf234d8e4b9
laumc98 <lanaca98@gmail.com>
2022-11-10 14:06:48 -0500

    Add rc_src&rc_syn already exists notifications queries

---
+++ rc_src_trxx_inv_notifications.sql
@@ -0,0 +1,16 @@
+/* AA : Channel's performance : rc_src_trxx_inv notifications : prod */ 
+SELECT
+    str_to_date(concat(yearweek(notif.date), ' Sunday'),'%X%V %W') AS 'date',
+    date(notif.date) as 'daily_date',
+    notif.AlfaID 
+FROM
+(
+    SELECT
+        date(career_advisor.created) AS `date`,
+        TRIM('"' FROM JSON_EXTRACT(career_advisor.context, '$.opportunityId')) as 'AlfaID'
+    FROM
+        career_advisor
+    WHERE
+        career_advisor.current = 'career-advisor-sourcing-already-exist'
+        AND career_advisor.notification_status = 'sent'
+) notif
