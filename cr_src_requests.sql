3f6c4ab6f0a72995795b0bfd99865a779f727137
laumc98 <lanaca98@gmail.com>
2022-11-23 16:30:12 -0500

    Add src requests by CR

---
+++ cr_src_requests.sql
@@ -0,0 +1,31 @@
+/* AA : channel performance : src requests by candidate recruiters: prod */ 
+SELECT
+    date(notifications.sent_at) AS date,
+    TRIM('"' FROM JSON_EXTRACT(notifications.context, '$.utmCampaign')) AS utm_campaign,
+    count(distinct notifications.id) AS requests
+FROM
+    notifications
+WHERE
+    (
+        (
+            notifications.template = 'career-advisor-sourcing-first-evaluation'
+            OR notifications.template = 'career-advisor-sourcing-first-evaluation-matrix-c'
+            OR notifications.template = 'career-advisor-sourcing-first-evaluation-matrix-a'
+            OR notifications.template = 'career-advisor-sourcing-first-evaluation-matrix-b'
+        )
+        AND date(notifications.sent_at) >= '2022-10-29'
+        AND notifications.status = 'sent'
+        AND TRIM('"' FROM JSON_EXTRACT(notifications.context, '$.utmCampaign')) IN (
+            'amdm',
+            'mcog',
+            'dffa',
+            'czp',
+            'jdpb',
+            'dmc',
+            'nsr',
+            'mmor'
+        )
+    )
+GROUP BY
+    date,
+    utm_campaign
