65cc18b79713ace3da6502ed2bd7c3ff1dc7ed36
laumc98 <lanaca98@gmail.com>
2022-11-25 09:25:37 -0500

    Create cr_mutual_m.sql

---
+++ cr_mutual_m.sql
@@ -0,0 +1,32 @@
+/* AA : channel performance : mutual matches by candidate recruiters: prod */ 
+SELECT
+    date(occh.created) AS 'date',
+    tc.utm_medium AS 'utm_medium',
+    tc.utm_campaign AS 'cr_campaign',
+    count(distinct occh.candidate_id) AS 'mutualm'
+FROM
+    opportunity_candidate_column_history occh
+    INNER JOIN opportunity_columns oc ON occh.to = oc.id
+    INNER JOIN opportunities o ON oc.opportunity_id = o.id
+    LEFT JOIN opportunity_candidates oca ON occh.candidate_id = oca.id
+    LEFT JOIN tracking_code_candidates tcc ON oca.id = tcc.candidate_id
+    LEFT JOIN tracking_codes tc ON tcc.tracking_code_id = tc.id
+WHERE
+    oc.name = 'mutual matches'
+    AND occh.created >= date(date_add(now(6), INTERVAL -3 month))
+    AND (tc.utm_campaign = 'amdm'
+        OR tc.utm_campaign = 'mcog'
+        OR tc.utm_campaign = 'dffa'
+        OR tc.utm_campaign = 'czp'
+        OR tc.utm_campaign = 'jdpb'
+        OR tc.utm_campaign = 'dmc'
+        OR tc.utm_campaign = 'nsr'
+        OR tc.utm_campaign = 'mmor'
+    )
+    AND tc.utm_medium IN ('src','rc_src','rc_src_trxx_inv','syn','rc_syn','rc_syn_trrx_inv','syn_paid','rc_syn_paid')
+GROUP BY
+    date(occh.created),
+    tc.utm_medium,
+    tc.utm_campaign
+ORDER BY
+    date(occh.created) ASC
