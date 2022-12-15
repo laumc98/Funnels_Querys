fbda34f5ea6e7a3766fe419c361bd2772ef04002
laumc98 <lanaca98@gmail.com>
2022-12-14 21:33:16 -0500

    Create cr_syn_requests.sql

---
+++ cr_syn_requests.sql
@@ -0,0 +1,31 @@
+/* AA : channel performance : syn requests by candidate recruiters: prod */ 
+SELECT
+    date(notifications.sent_at) AS date,
+    TRIM('"' FROM JSON_EXTRACT(notifications.context, '$.opportunityId')) AS AlfaID,
+    TRIM('"' FROM JSON_EXTRACT(notifications.context, '$.utmCampaign')) AS cr_campaign,
+    count(distinct notifications.id) AS requests
+FROM
+    notifications
+WHERE
+    (
+        (
+            notifications.template = 'career-advisor-syndication-first-evaluation'
+            OR notifications.template = 'career-advisor-syndication-already-exist'
+        )
+        AND date(notifications.sent_at) >= '2022-10-29'
+        AND notifications.status = 'sent'
+        AND TRIM('"' FROM JSON_EXTRACT(notifications.context, '$.utmCampaign')) IN (
+            'amdm',
+            'mcog',
+            'dffa',
+            'czp',
+            'jdpb',
+            'dmc',
+            'nsr',
+            'mmor'
+        )
+    )
+GROUP BY
+    date,
+    AlfaID,
+    cr_campaign
