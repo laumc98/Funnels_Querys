b6d1f8af69ade85506981da501dc07e79cf8b862
laumc98 <lanaca98@gmail.com>
2022-12-28 16:29:43 -0500

    Create search_notifications.sql

---
+++ search_notifications.sql
@@ -0,0 +1,11 @@
+/* AA : Channel's performance : Search notifications : prod */ 
+SELECT
+    (CAST(date_trunc('week',CAST((sp."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) AS "date",
+    SUM(results_open) AS weekly
+FROM
+    "snowplow"."com_torrelabs_discovery_search_performance_5" sp
+WHERE
+    sp."search_target" = 'opportunities'
+    AND date(sp."root_tstamp") > '2022-01-08'
+GROUP BY
+    1
