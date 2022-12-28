2f5cf10596679d4a36b6dc8cfcd5433c1e36f2ff
laumc98 <lanaca98@gmail.com>
2022-12-28 16:36:32 -0500

    Update search_notifications.sql

--- search_notifications.sql
+++ search_notifications.sql
@@ -1,7 +1,7 @@
 /* AA : Channel's performance : Search notifications : prod */ 
 SELECT
     (CAST(date_trunc('week',CAST((sp."root_tstamp" + (INTERVAL '1 day')) AS timestamp)) AS timestamp) + (INTERVAL '-1 day')) AS "date",
-    SUM(results_open) AS weekly
+    SUM(results_open) AS weekly_search
 FROM
     "snowplow"."com_torrelabs_discovery_search_performance_5" sp
 WHERE
