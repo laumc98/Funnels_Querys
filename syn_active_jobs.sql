1f0023009489785e16c2a5cf82ae68d1c43d53a6
laumc98 <lanaca98@gmail.com>
2023-01-31 13:52:53 -0500

    Update syn_active_jobs.sql

--- syn_active_jobs.sql
+++ syn_active_jobs.sql
@@ -4,8 +4,11 @@ SELECT
     opportunity_channels.opportunity_reference_id as 'AlfaID'
 FROM 
     opportunity_channels
+    INNER JOIN opportunity ON opportunity_channels.opportunity_reference_id = opportunity.ref_id
 WHERE 
     opportunity_channels.channel = 'EXTERNAL_NETWORKS'
+    AND (opportunity.max_proficiency <> 'EXPERT'
+        AND opportunity.max_proficiency <> 'MASTER')
 GROUP BY
     date(opportunity_channels.created),
     opportunity_channels.opportunity_reference_id
