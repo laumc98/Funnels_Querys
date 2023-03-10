e0924d088fb9643f11afbdb15e48084cc52d6669
laumc98 <lanaca98@gmail.com>
2023-03-10 15:39:06 -0500

    Create applications_info.sql

---
+++ applications_info.sql
@@ -0,0 +1,13 @@
+/* AA : Channel's performance : app info : prod */ 
+SELECT
+    opportunity_candidates.interested AS 'application date',
+    opportunity_candidates.opportunity_id AS 'ID',
+    opportunities.fulfillment AS 'fulfillment',
+    people.gg_id AS 'gg_id'
+FROM 
+    opportunity_candidates
+    LEFT JOIN people ON opportunity_candidates.person_id = people.id
+    LEFT JOIN opportunities ON opportunity_candidates.opportunity_id = opportunities.id
+WHERE 
+    opportunity_candidates.interested IS NOT NULL 
+    AND opportunity_candidates.interested > '2023-03-06'
