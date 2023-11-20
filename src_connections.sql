dacb2d1a1672cd447e0e99b67e73e2fb4bba9e24
laumc98 <lanaca98@gmail.com>
2023-11-20 17:45:27 -0500

    Create src_connections.sql

---
+++ src_connections.sql
@@ -0,0 +1,13 @@
+/* AA : Channel's performance : sourcing connections : prod */ 
+SELECT 
+    tasks.*,
+    opportunities.opportunity_id
+FROM 
+    tasks
+    LEFT JOIN opportunities ON tasks.opportunity_id = opportunities.id
+WHERE 
+    (tasks.trigger = 'sourcing-connection-request'
+        OR tasks.trigger = 'sourcing-connection-request-agile'
+        OR tasks.trigger = 'sourcing-connection-request-boost'
+        OR tasks.trigger = 'sourcing-connection-request-pro'
+        OR tasks.trigger = 'sourcing-connection-request-ss')
