4b4988c3e830f664680d17be2472aa06dccf84ee
laumc98 <lanaca98@gmail.com>
2023-11-21 16:24:56 -0500

    Update src_connections_brought.sql

--- src_connections_brought.sql
+++ src_connections_brought.sql
@@ -1,7 +1,9 @@
 /* AA : Channel's performance : src connections brought : prod */ 
 SELECT 
-    master_chief_events.*
+    master_chief_events.*,
+    sourcing_connections.linkedin_username
 FROM 
     master_chief_events
+    LEFT JOIN sourcing_connections ON master_chief_events.target_user_gg_id = sourcing_connections.candidate_gg_id
 WHERE 
     master_chief_events.event = 'sourcing_connection_status_updated'
